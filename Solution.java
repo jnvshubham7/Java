6d6b5dd366d1af1584c7c840b21769cbf61397e2
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2024-09-29 18:06:57 +0530

    u

--- Solution.java
+++ Solution.java
@@ -1,19 +1,63 @@
-import java.util.*;
+import java.util.ArrayList;
+
+class Car {
+    private int carId;
+    private String carName;
+    private String fuelType;
+
+    public Car(int carId, String carName, String fuelType) {
+        this.carId = carId;
+        this.carName = carName;
+        this.fuelType = fuelType;
+    }
+
+    public int getCarId() {
+        return carId;
+    }
+
+    public void setCarId(int carId) {
+        this.carId = carId;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    @Override
+    public String toString() {
+        return "Car ID: " + carId + ", Name: " + carName + ", Fuel: " + fuelType;
+    }
+}
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String num = sc.nextLine();
-        int sum = 0;
-        for (int i = 0; i < num.length(); i++) {
-            sum+= Math.pow(Integer.parseInt(String.valueOf(num.charAt(i))),3);   
+        // Create an ArrayList of Car objects
+        ArrayList<Car> cars = new ArrayList<>();
+        cars.add(new Car(1, "Toyota", "Petrol"));
+        cars.add(new Car(2, "Honda", "Diesel"));
+        cars.add(new Car(3, "Ford", "Electric"));
+
+        // Remove the second car (Honda)
+        cars.remove(1);
+
+        // Print the list after removal
+        for (Car car : cars) {
+            System.out.println(car);
         }
-        if(sum == Integer.parseInt(num))
-        {
-            System.out.println("Yes, the number is an Armstrong number.");
+
+        int indx = 1;
+
+        for (Car car : cars) {
+            car.setCarId(indx);
+            indx++;
         }
-        else{
-            System.out.println("No, the number is not an Armstrong number.");
+
+        for (Car car : cars) {
+            System.out.println(car);
         }
     }
 }
--- TCS IPA/AutonomousCar/Solution.java
+++ TCS IPA/AutonomousCar/Solution.java
@@ -5,6 +5,8 @@ public class Solution {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     AutonomousCar[] cars = new AutonomousCar[4];
+    
+    // Reading input for 4 Autonomous Cars
     for (int i = 0; i < cars.length; i++) {
       int carId = sc.nextInt();
       sc.nextLine();
@@ -15,29 +17,69 @@ public class Solution {
       String environment = sc.nextLine();
       cars[i] = new AutonomousCar(carId, brand, noOfTestsConducted, noOfTestsPassed, environment);
     }
+    
+    // Input for environment to find test passed
     String env = sc.nextLine();
+    
+    // Input for brand to update grade
     String brand = sc.nextLine();
+    
+    // Call the method to find number of tests passed in the given environment
     int testsPassed = findTestPassedByEnv(cars, env);
     if (testsPassed == 0)
       System.out.println("There are no tests passed in this particular environment");
     else
       System.out.println(testsPassed);
+    
+    // Call the method to update the car's grade by brand
     AutonomousCar updatedCar = updateCarGrade(cars, brand);
     if (updatedCar == null)
       System.out.println("No Car is available with the specified brand");
     else
       System.out.println(updatedCar.getBrand() + "::" + updatedCar.getGrade());
+    
+    sc.close();  // Closing the scanner
   }
 
+  // Method to find total tests passed by environment
   public static int findTestPassedByEnv(AutonomousCar[] cars, String env) {
-   
+    int sum = 0;
+    for (AutonomousCar car : cars) {
+      if (car.getEnvironment().equalsIgnoreCase(env)) {
+        sum += car.getNoOfTestsPassed();
+      }
+    }
+    return sum;
   }
 
+  // Method to update car's grade based on its brand
   public static AutonomousCar updateCarGrade(AutonomousCar[] cars, String brand) {
-   
+    for (AutonomousCar car : cars) {
+      if (car.getBrand().equalsIgnoreCase(brand)) {
+        int noOfTestsConducted = car.getNoOfTestsConducted();
+        int noOfTestsPassed = car.getNoOfTestsPassed();
+        
+        // Avoid division by zero
+        if (noOfTestsConducted == 0) {
+          car.setGrade("No Grade (No tests conducted)");
+        } else {
+          int rating = (noOfTestsPassed * 100) / noOfTestsConducted;
+          
+          // Set grade based on the rating
+          if (rating >= 80) {
+            car.setGrade("A1");
+          } else {
+            car.setGrade("B2");
+          }
+        }
+        return car;  // Return the car once the grade is updated
+      }
+    }
+    return null;  // Return null if no car matches the brand
   }
 }
 
+// AutonomousCar class to represent each autonomous car
 class AutonomousCar {
   private int carId, noOfTestsConducted, noOfTestsPassed;
   private String brand, environment, grade;
